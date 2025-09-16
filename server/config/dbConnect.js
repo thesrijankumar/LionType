30fa067122217785d493e60c102f374c62be56b5
Srijan Kumar <thesrijankumar@proton.me>
2025-09-16 22:54:46 +0530

    v0.7 ApiError added

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import ApiError from "../utils/ApiError";
 
 const dbConnect = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/LionType`);
         console.log("Database connection success!~")
     } catch (error) {
-        console.log(error)
+        console.log(new ApiError(500, "Internal Server Error: Database connection failed"))
     }
 }
 
--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,1 +1,21 @@
-// class ApiError extends Error 
+class ApiError extends Error {
+    constructor( 
+        statuscode ,
+        message = "Something went wrong",
+        errors = { name: `Code Red MF!! ${statuscode}`, message: message},
+        stack = null
+    ) {
+        super(message)
+        this.statuscode = statuscode;
+        this.errors = errors;
+        this.success = false;
+
+        if(stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export default ApiError;
